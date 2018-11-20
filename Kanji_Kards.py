dcbaced146cfb8042661723aadd01559a0f29bcb
pteridophobia <31938282+pteridophobia@users.noreply.github.com>
2018-11-20 01:03:51 -0800

    full KanjiKard list implemented

--- Kanji_Kards.py
+++ Kanji_Kards.py
@@ -16,148 +16,11 @@
 import re
 import struct
 from struct import *
+from bs4 import BeautifulSoup
 
-
-
-def hiragana_to_romanji(hira):
-    romanji = str()
-    str1 = ['']*(len(hira))
-    i = 0
-    next_rep = -1
-    def hiragana_dict(hira):
-        return {
-            'あ': 'a',
-            'い': 'i',
-            'う': 'u',
-            'え': 'e',
-            'お': 'o',
-            'か': 'ka',
-            'き': 'ki',
-            'く': 'ku',
-            'け': 'ke',
-            'こ': 'ko',
-            'さ': 'sa',
-            'し': 'shi',
-            'す': 'su',
-            'せ': 'se',
-            'そ': 'so',
-            'た': 'ta',
-            'ち': 'chi',
-            'つ': 'tsu',
-            'て': 'te',
-            'と': 'to',
-            'な': 'na',
-            'に': 'ni',
-            'ぬ': 'nu',
-            'ね': 'ne',
-            'の': 'no',
-            'は': 'ha',
-            'ひ': 'hi',
-            'ふ': 'fu',
-            'へ': 'he',
-            'ほ': 'ho',
-            'ま': 'ma',
-            'み': 'mi',
-            'む': 'mu',
-            'め': 'me',
-            'も': 'mo',
-            'や': 'ya',
-            'ゆ': 'yu',
-            'よ': 'yo',
-            'ら': 'ra', 
-            'り': 'ri',
-            'る': 'ru',
-            'れ': 're',
-            'ろ': 'ro',
-            'わ': 'wa',
-            'ゐ': 'wi',
-            'ゑ': 'we',
-            'を': 'wo',
-            'ん': 'n',
-            'が': 'ga',
-            'ぎ': 'gi',
-            'ぐ': 'gu',
-            'げ': 'ge',
-            'ご': 'go',
-            'ざ': 'za',
-            'じ': 'ji',
-            'ず': 'zu',
-            'ぜ': 'ze',
-            'ぞ': 'zo',
-            'だ': 'da',
-            'ぢ': 'dji',
-            'づ': 'dzu',
-            'で': 'de',
-            'ど': 'do',
-            'ば': 'ba',
-            'び': 'bi',
-            'ぶ': 'bu',
-            'べ': 'be',
-            'ぼ': 'bo',
-            'ぱ': 'pa',
-            'ぴ': 'pi',
-            'ぷ': 'ぷ',
-            'ぺ': 'pe',
-            'ぽ': 'po',
-            'きゃ': 'kya',
-            'きゅ': 'kyu',
-            'きょ': 'kyo',
-            'しゃ': 'sha',
-            'しゅ': 'shu',
-            'しょ': 'sho',
-            'ちゃ': 'cha',
-            'ちゅ': 'chu',
-            'ちょ': 'cho',
-            'にゃ': 'nya',
-            'にゅ': 'nyu',
-            'にょ': 'nyo',
-            'ひゃ': 'hya',
-            'ひゅ': 'hyu',
-            'ひょ': 'hyo',
-            'みゃ': 'mya',
-            'みゅ': 'myu',
-            'みょ': 'myo',
-            'りゃ': 'rya',
-            'りゅ': 'ryu',
-            'りょ': 'ryo',
-            'ぎゃ': 'gya',
-            'ぎゅ': 'gyu',
-            'ぎょ': 'gyo',
-            'じゃ': 'ja',
-            'じゅ': 'ju',
-            'じょ': 'jo',
-            'ぢゃ': 'ja',
-            'ぢゅ': 'ju',
-            'ぢょ': 'jo',
-            'びゃ': 'bya',
-            'びゅ': 'byu',
-            'びょ': 'byo',
-            'ぴゃ': 'pya',
-            'ぴゅ': 'pyu',
-            'ぴょ': 'pyo',
-            'ょ': '',
-            'ゅ': '', 
-            'ゃ': '',
-            'っ': '',
-            '\n': ''
-        }[hira]
-    for c in hira:
-        str1[i] = c
-        if c == 'ょ' or c == 'ゅ' or c == 'ゃ':
-            str1[i-1] = str1[i-1] + str1[i]
-        i += 1
-    i = 0
-    for h in str1:
-        if h == 'っ':
-            next_rep = 1
-        romanji = romanji + hiragana_dict(str1[i])
-        if next_rep == 1:
-            next_rep = 0
-            temp = hiragana_dict(str1[i+1])[0:1]
-            romanji = romanji + hiragana_dict(str1[i+1])[0:1] + hiragana_dict(str1[i])
-        i+=1
-    return romanji
-print(hiragana_to_romanji("はっぴゃっく"))
+import re
+import struct
+from struct import *
 
 
 class KanjiKard:
@@ -166,8 +29,6 @@ class KanjiKard:
     reading = ""    # The hiragana reading of character
     romanji = ""    # The romanji (english alphabet) reading
     english = ""    # The english meaning of the character
-    english2 = ""   # Other meanings
-    english3 = ""   # Other meanings
     grade_level = 1 # The grade level for which the character is learned
     num_attempts = 0# The number of attempts the user has used for the character
     num_right = 0   # The number of correct attempts
@@ -194,46 +55,66 @@ class KanjiKard:
 .prev = 
 """
 #num_attempts, num_right, and num_wrong should always be initialized to 0 unless user data is loaded
-one = KanjiKard() # This is the head to the list of KanjiKard s
-one.kanji = "一"
-one.reading = "いち"
-one.romanji = "ichi"
-one.english = "one" 
-one.english2 = ""
-one.english3 = ""
-one.grade_level = 1
-one.next = KanjiKard()
-head = KanjiKard()
-head = one
+
+
+head = KanjiKard() # This is the head to the list of KanjiKards 
+head.kanji = '頭'
+head.english = 'head'
+head.grade_level = 2
+head.reading = 'あたま'
+head.romanji = 'atama'
+head.next = KanjiKard()
+
 temp = KanjiKard()
 temp = head
 count = 0
 after = KanjiKard()
-while count < 3:
-    
-    if count == 0:
-        after.kanji = "十"
-        after.reading = "じゅう"
-        after.romanji = "juu"
-        after.english = "ten"
-        after.grade_level = 1
-    elif count == 1:
-        after.kanji = "七"
-        after.reading = "asf"
-        after.romanji = "nana"
-        after.english = "seven"
-        after.grade_level = 1
-    elif count == 2:
-        after.kanji = "五"
-    #print(after.kanji)
-    after.prev = temp
-    temp.next = after
-    temp = after
-    after.next = KanjiKard()
-    after = after.next
-    count += 1
 
+kanji_file = open("KanjiKard_list_V1.txt", "r")
+i = 0
+it = 0
+for line in kanji_file:
+    kanji_in = line
+    kanji_in = kanji_in[0:len(kanji_in)-1]
+    if line == '\n':
+        print("New line")
+    elif line == None:
+        print("None")
+    elif it == 0:
+        after.kanji = kanji_in
+       # print(after.kanji)
+        it += 1
+    elif it == 1:
+        after.reading = kanji_in
+        #print(after.reading)
+        it += 1
+    elif it == 2:
+        after.romanji = kanji_in
+        #print(after.romanji)
+        it += 1
+    elif it == 3:
+        after.english = kanji_in
+        #print(after.english)
+        it += 1
+    elif it == 4:
+        if kanji_in == 9:
+            after.grade_level = "taught in junior high"
+            #print(after.grade_level)
+            it += 1
+        else:
+            after.grade_level = kanji_in
+            #print(after.grade_level)
+            it += 1
+    elif it == 5:
+        it = 0
+        after.prev = temp
+        temp.next = after
+        temp = after
+        after.next = KanjiKard()
+        after = after.next
+    i+=1
 
+print(i)
 
 def print_all(head): #prints all members of each KanjiKard
     temp = KanjiKard()
@@ -267,12 +148,13 @@ def search_for_kanji(head, target): #searches list for target kanji and returns 
             break
         if temp_kanji == target:
             print("Found", target)
-            return temp;
+            return temp
         temp = temp.next
         temp_kanji = temp.kanji
 testS = KanjiKard()
 testS = search_for_kanji(head, target = "七")
-print(testS.kanji, ":Search test")
+if testS != None:
+    print(testS.kanji, ":Search test")
 
 
 print("now we print all kanji\n")

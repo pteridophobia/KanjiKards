785102b261f4feffe265ea22d55005461b660cec
pteridophobia <31938282+pteridophobia@users.noreply.github.com>
2019-01-25 14:03:39 -0800

    added load and save functionality
    NOT YET TESTED. Also User class implemented
    def no_save_file(head): , def save(head): , def load(head): , def load_user(head):
    TODO:: implement to_study list and methods, add to menu

--- Kanji_Kards.py
+++ Kanji_Kards.py
@@ -30,6 +30,7 @@ class KanjiKard:
     romanji = ""    # The romanji (english alphabet) reading
     english = ""    # The english meaning of the character
     grade_level = 1 # The grade level for which the character is learned
+    learned = False # False until user has gotten a right/wrong ratio of 2
     num_attempts = 0# The number of attempts the user has used for the character
     num_right = 0   # The number of correct attempts
     num_wrong = 0   # The number of incorrect attempts
@@ -39,15 +40,30 @@ class KanjiKard:
     def __init__(self):
         self.next = None# pointer to next
         self.prev = None# pointer to previous
+
+class User:
+    def __init__(self):
+        self.current_learned = []
+        self.not_learned = []
+        self.to_study = []
+    def add_learned(self, kard):
+        self.current_learned.append(kard)
+    def add_not_learned(self, kard):
+        self.not_learned.append(kard)
+    def add_to_study(self, kard): #///////TODO
+        self.to_study.append(kard)
+
 """
  = KanjiKard()
 .kanji = ""
 .reading = ""
 .romanji = ""
 .english = ""
-.english2 = ""
-.english3 = ""
 .grade_level = 1
+.learned = False
+.num_attempts = 0
+.num_right = 0
+.num_wrong = 0
 .daily_correct = 0
 .daily_wrong = 0
 .daily_easy = 0
@@ -65,56 +81,164 @@ head.reading = 'あたま'
 head.romanji = 'atama'
 head.next = KanjiKard()
 
-temp = KanjiKard()
-temp = head
-count = 0
-after = KanjiKard()
 
-kanji_file = open("KanjiKard_list_V1.txt", "r")
-i = 0
-it = 0
-for line in kanji_file:
-    kanji_in = line
-    kanji_in = kanji_in[0:len(kanji_in)-1]
-    if line == '\n':
-        print("New line")
-    elif line == None:
-        print("None")
-    elif it == 0:
-        after.kanji = kanji_in
-       # print(after.kanji)
-        it += 1
-    elif it == 1:
-        after.reading = kanji_in
-        #print(after.reading)
-        it += 1
-    elif it == 2:
-        after.romanji = kanji_in
-        #print(after.romanji)
-        it += 1
-    elif it == 3:
-        after.english = kanji_in
-        #print(after.english)
-        it += 1
-    elif it == 4:
-        if kanji_in == 9:
-            after.grade_level = "taught in junior high"
-            #print(after.grade_level)
-            it += 1
-        else:
-            after.grade_level = kanji_in
-            #print(after.grade_level)
-            it += 1
-    elif it == 5:
-        it = 0
-        after.prev = temp
-        temp.next = after
-        temp = after
-        after.next = KanjiKard()
-        after = after.next
-    i+=1
 
 print(i)
+
+def no_save_file(head):
+    temp = KanjiKard()
+    temp = head
+    count = 0
+    after = KanjiKard()
+
+    kanji_file = open("KanjiKard_list_V1.txt", "r")
+    it = 0
+    for line in kanji_file:
+        kanji_in = line
+        kanji_in = kanji_in[0:len(kanji_in)-1]
+        if line == '\n':
+            print("New line")
+        elif line == None:
+            print("None")
+        elif it == 0:
+            after.kanji = kanji_in
+        # print(after.kanji)
+            it += 1
+        elif it == 1:
+            after.reading = kanji_in
+            #print(after.reading)
+            it += 1
+        elif it == 2:
+            after.romanji = kanji_in
+            #print(after.romanji)
+            it += 1
+        elif it == 3:
+            after.english = kanji_in
+            #print(after.english)
+            it += 1
+        elif it == 4:
+            if kanji_in == 9:
+                after.grade_level = "taught in junior high"
+                #print(after.grade_level)
+                it += 1
+            else:
+                after.grade_level = kanji_in
+                #print(after.grade_level)
+                it += 1
+        elif it == 5:
+            it = 0
+            after.prev = temp
+            temp.next = after
+            temp = after
+            after.next = KanjiKard()
+            after = after.next
+    kanji_file.close()
+
+def save(head):
+    save_file = open("KanjiKard_save.txt", "w")
+    temp = KanjiKard()
+    temp = head
+    while temp.kanji != None:
+        if temp.next == None:
+            save_file.close()
+            break
+        save_file.write(temp.kanji)
+        save_file.write('\n')
+        save_file.write(temp.reading)
+        save_file.write('\n')
+        save_file.write(temp.romanji)
+        save_file.write('\n')
+        save_file.write(temp.english)
+        save_file.write('\n')
+        save_file.write(temp.grade_level)
+        save_file.write('\n')
+        save_file.write(temp.learned)
+        save_file.write('\n')
+        save_file.write(temp.num_atempts)
+        save_file.write('\n')
+        save_file.write(temp.num_right)
+        save_file.write('\n')
+        save_file.write(temp.num_wrong)
+        save_file.write('\n')
+        save_file.write(temp.daily_correct)
+        save_file.write('\n')
+        save_file.write(temp.daily_wrong)
+        save_file.write('\n')
+        save_file.write(temp.daily_easy)
+        save_file.write('\n')
+        temp = temp.next
+
+def load(head):
+    load_file = open("KanjiKard_save.txt", "r")
+    temp = KanjiKard()
+    temp = head
+    after = KanjiKard()
+    count = 0
+    it = 0
+    for line in load_file:
+        in_line = line
+        in_line = in_line[0:len(in_line) - 1]
+        if line == '\n':
+            continue
+        elif line == None:
+            continue
+        elif it == 0:
+            after.kanji = in_line
+            it += 1
+        elif it == 1:
+            after.reading = in_line
+            it += 1
+        elif it == 2:
+            after.romanji = in_line
+            it += 1
+        elif it == 3:
+            after.english = in_line
+            it += 1
+        elif it == 4:
+            after.grade_level = in_line
+            it += 1
+        elif it == 5:
+            after.learned = in_line
+            it += 1
+        elif it == 6:
+            after.num_attempts = in_line
+            it += 1
+        elif it == 7:
+            after.num_right = in_line
+            it += 1
+        elif it == 8:
+            after.num_wrong = in_line
+            it += 1
+        elif it == 9:
+            after.daily_correct = in_line
+            it += 1
+        elif it == 10:
+            after.daily_wrong = in_line
+            it += 1
+        elif it == 11:
+            after.daily_easy = in_line
+            it += 1
+        elif it == 12:
+            it = 0
+            after.prev = temp
+            temp.next = after
+            temp = after
+            after.next = KanjiKard()
+            after = after.next
+    load_file.close()
+
+def load_user(head):
+    temp = KanjiKard()
+    temp = head
+    new_user = user()
+    while temp.kanji != None:
+        if temp.next == None:
+            return new_user
+        elif temp.learned == False:
+            new_user.add_not_learned(temp)
+        elif temp.learned = True:
+            new_user.add_learned(temp)
+                    
 
 def print_all(head): #prints all members of each KanjiKard
     temp = KanjiKard()
